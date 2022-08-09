f53ab09514d8e826d5666063a18e7e222b8bbdfb
Thomas Schuerlein <tschuerl@amazon.com>
2022-08-09 09:44:26 -0700

    adding tool project for CLI

---
+++ TypeInjections/Tool/Tool.fs
@@ -0,0 +1,19 @@
+﻿
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT
+
+namespace Tool
+
+open System.IO
+open System
+open TypeInjections
+
+module Tester = 
+                           
+
+    [<EntryPoint>]
+    let main (argv: string array) =
+        
+        Utils.log "***** Entering Tool.fs"
+        
+        0
