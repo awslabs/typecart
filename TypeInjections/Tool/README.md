dbe27989134587971c32fbd22a1223d3ffc877b4
Thomas Schuerlein <tschuerl@amazon.com>
2022-08-10 20:59:47 -0700

    define features for dotnet tool

---
+++ TypeInjections/Tool/README.md
@@ -0,0 +1,8 @@
+## typeCart features
+typeCart offers multiple ways to compare two projects
+- projects on local computer:
+    * `$ typecart -p [OldFolderPath] [NewFolderPath] [OutputPath]`
+- git commits on a remote repository:
+    * `$ typecart -r https://gitub.com/[org]/[repo].git [git commit SHA1] [git commit SHA1] [OutputPath]`
+- compare git commits on local repostory:
+    * `$ typcart -l [git commit SHA1] [git commit SHA1] [OutputPath]`
--- TypeInjections/Tool/Tool.fs
+++ TypeInjections/Tool/Tool.fs
@@ -8,12 +8,46 @@ open System.IO
 open System
 open TypeInjections
 
-module Tester = 
+module Tool =
+    
+    // based off command line flags, create and provide path for input/output folders
+    let interpretFlag  (argvList: string list) =
+        let flag = argvList.Item(0)
+      
+        // -p [inputPath] [inputPath] [outputPath]
+        if flag.Contains("-p") then
+          
+            if argvList.Length <> 4 then
+              failwith "usage: program -p OLD NEW OUTPUTFOLDER"
+            
+            Console.WriteLine("***** local project comparison")
+          
+        // -l [git commit hash] [git commit hash] [OutputPath]
+        elif flag.Contains("-l") then
+          
+            if argvList.Length <> 4 then
+              failwith "usage: program -l OLD_COMMIT NEW_COMMIT OUTPUTFOLDER"
+          
+            Console.WriteLine("***** git commit")
+     
+        // -r [github repo url] [github commit hash] [github commit hash] [OutputPath]
+        elif flag.Contains("-r") then
+            
+            if argvList.Length <> 5 then
+              failwith "usage: program -p OLD NEW OUTPUTFOLDER"
+            
+            Console.WriteLine("***** remote github")
+          
+        else failwith "flag not found please try again"
                            
 
     [<EntryPoint>]
     let main (argv: string array) =
         
         Utils.log "***** Entering Tool.fs"
+        
+        let argvList = argv |> Array.toList
+        
+        interpretFlag argvList 
         
         0
