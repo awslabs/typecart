311a5869fbc0cc9f9fc38e3c582aca219c22ca87
Thomas Schuerlein <tschuerl@amazon.com>
2022-08-24 13:35:36 -0700

    Implement CLI verbs, remove options

--- TypeInjections/Tool/Tool.fs
+++ TypeInjections/Tool/Tool.fs
@@ -11,9 +11,35 @@ open CommandLine
 
 module Tool =
 
+
+    [<Verb("local", HelpText = "typecart local [old project path] [new project path] [outputPath]")>]
+    type LocalOptions =
+        { [<Option('o', "old", Required = false, HelpText = "folder path of old project")>]
+          oldFolder: string
+          [<Option('n', "new", Required = false, HelpText = "folder path of new project")>]
+          newFolder: string
+          [<Option('p', "output", Required = false, HelpText = "path of folder to place typeCart files")>]
+          outputFolder: string
+          [<Option('f', "file", Required = false, HelpText = "specify filename(s) you want to run typeCart on")>]
+          file: seq<string> }
+
+    [<Verb("git", HelpText = "typecart git [old commit] [new commit] [outputPath]")>]
+    type GitOptions =
+        { [<Option('o', "old", Required = true, HelpText = "commit hash of old version")>]
+          oldHash: string
+          [<Option('n', "new", Required = true, HelpText = "commit hash of new version")>]
+          newHash: string
+          [<Option('p', "output", Required = true, HelpText = "path of folder to place typeCart files")>]
+          outputFolder: string
+          [<Option('e', "entrypoint", Required = false, HelpText = "specify the filename(s) you want to run typeCart on")>]
+          file: seq<string>
+          [<Option('c', "clone", Required = false, HelpText = "input a git URL")>]
+          url: string
+          [<Option('f', "force", Required = false, HelpText = "overwrite the Old and New folders in the Output folder")>]
+          force: bool }
+
     //NOTE 'location' refers to either the github url OR path to repo on local computer
     let checkoutCommit (hash: string) (location: string) (outputPath: string) : unit =
-
         Repository.Clone(location, outputPath) |> ignore
         // get Repository object of whatever was cloned above
         let newRepo = new Repository(outputPath)
@@ -21,77 +47,43 @@ module Tool =
         let commitRepo = newRepo.Lookup<Commit>(hash)
         Commands.Checkout(newRepo, commitRepo) |> ignore
 
+    let runGit (git: GitOptions) : unit =
+        Utils.log "***** git commit"
 
-    type options =
-        { [<Option('d', "delete", Required = false, HelpText = "use option to remove the New and Old folders")>] delete: bool
-          [<Option('l', "local", Required = false, HelpText = "[old project path] [new project path] [outputPath]")>] local: seq<string>
-          [<Option('g', "git", Required = false, HelpText = "[old commit] [new commit] [outputPath]")>] git: seq<string>
-          [<Option('r', "remote", Required = false, HelpText = "[github repo url] [new commit] [old commit] [outputPath]")>] remote: seq<string>
-          [<Option(HelpText = "Prints all messages to standard output.")>] verbose: bool
-          }
+        let commit1, commit2 = git.oldHash, git.newHash
+        let path = git.outputFolder
+        let pathOld = $"{path}/Old"
+        let pathNew = $"{path}/New"
 
-    (* command line parser has group arguments into what option it is associated with
-     (local option has a list of arguments that follow it, as does git and remote).
-     If command line option has arguments, we need to organize arguments and run typeCart on them.
-     Run typeCart only once per command line call (we can't run a remote and local project on same command line call)
-     *)
-    let interpretFlags  (local: string list) (git: string list) (remote: string list) : string * string * string =
+        let location =
+            match git.url.Length with
+            // empty git url means user wants to run typeCart in current directory
+            | 0 -> Directory.GetCurrentDirectory()
+            // if there is a repo URL, use it
+            | _ -> Utils.log $"Path to repo is {path}"; git.url
 
-        if not local.IsEmpty then
-            Utils.log "***** local project comparison"
-            local.[0], local.[1], local.[2]
-        elif not git.IsEmpty then
-            Utils.log "***** git commit"
-            // location of repo
-            let location = Directory.GetCurrentDirectory()
-
-            let commit1, commit2 = git.[0], git.[1]
-            let path = git.[2]
-            let pathOld = $"{path}/Old"
-            let pathNew = $"{path}/New"
-
-            // lib2git needs an empty folder to store the repo clones
-            for a in [ pathOld; pathNew ] do
-                if Directory.Exists(a) then
+        // lib2git needs an empty folder to store the repo clones
+        for a in [ pathOld; pathNew ] do
+            if Directory.Exists(a) then
+                // if user lets us delete existing folder
+                if git.force then
                     Directory.Delete(a, true)
+                // else break program
+                else
+                    failwith
+                        $"{a} already exists. We need an empty folder to place checkout git repo. Choose different output folder or choose --force to automatically overwrite folder data"
 
-            checkoutCommit commit1 location pathOld
-            checkoutCommit commit2 location pathNew
+        checkoutCommit commit1 location pathOld
+        checkoutCommit commit2 location pathNew
 
-            pathOld, pathNew, $"{path}/Output"
-
-        elif not remote.IsEmpty then
-            Utils.log "***** remote github"
-
-            let path = remote.[3]
-            let pathOld = $"{path}/Old"
-            let pathNew = $"{path}/New"
-            let url = remote.[0]
-            let commit1 = remote.[1]
-            let commit2 = remote.[2]
-
-            for a in [ pathOld; pathNew ] do
-                if Directory.Exists(a) then
-                    Directory.Delete(a, true)
-
-            checkoutCommit commit1 url pathOld
-            checkoutCommit commit2 url pathNew
+        Utils.log $"{pathOld}, {pathNew}, {path}/Output"
 
 
-            pathOld, pathNew, $"{path}/Output"
+    let runLocal (local: LocalOptions) : unit =
 
-        else
-            failwith "unable to read flags"
-            
-    // organize arguments with the flag they associated with 
-    let run opt =
-        let local = opt.local |> Seq.toList
-        let git = opt.git |> Seq.toList
-        let remote = opt.remote |> Seq.toList
-        // given the list of arguments associated with each flag, run typeCart
-        let oldFolder, newFolder, outputFolder = interpretFlags local git remote
+        Utils.log "***** local project comparison"
+        Utils.log $"{local.oldFolder}, {local.newFolder}, {local.outputFolder}"
 
-        Utils.log $"old folder is {oldFolder}, new folder is {newFolder}, output folder is {outputFolder}"
 
     [<EntryPoint>]
     let main (argv: string array) =
@@ -99,13 +91,18 @@ module Tool =
         Utils.log "***** Entering Tool.fs"
 
         let result =
-            Parser.Default.ParseArguments<options>(argv)
+            Parser.Default.ParseArguments<LocalOptions, GitOptions> argv
 
-        //TODO get a better understanding of result
+
         match result with
-        | :? (Parsed<options>) as parsed -> run parsed.Value
-        | :? (NotParsed<'a>) as notParsed -> Utils.log $"Here is non parsed arguments {notParsed}"
-        | _ as unknown -> Utils.log $"don't understand this argument {unknown}"  
+        | :? (Parsed<obj>) as command ->
+            match command.Value with
+            | :? LocalOptions as opts -> runLocal opts
+            | :? GitOptions as opts -> runGit opts
+            | _ -> ()
+
+        | :? (NotParsed<obj>) as value -> ()
+        | _ -> ()
 
 
         0
