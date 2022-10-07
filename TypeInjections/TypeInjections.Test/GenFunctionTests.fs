cdfaf7b47881667f46ee3505735b32ded5d084b2
Hira Syeda <syedahir@amazon.com>
2022-10-07 11:38:02 -0700

    Fix build warnings in .Test.fsproj

---
+++ TypeInjections/TypeInjections.Test/GenFunctionTests.fs
@@ -0,0 +1,20 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT
+
+namespace TypeInjections.Test
+
+module GenFunctionTests =
+    open NUnit.Framework
+
+    type internal Marker =
+        interface
+        end
+
+    let moduleName = typeof<Marker>.DeclaringType.Name
+    // set the module name, alternatively we could look into determining
+    // the calling module via reflection in TestUtils
+        
+    let testRunner =
+        TestUtils.testRunnerGen
+        
+        
