awslabs/typecart

commit a561f98ef82601232efe979f05602f236488e9e8

Change the type of lhs in ImportEquals to string

TypeInjections/TypeInjections/Analysis.fs
@@ -211,11 +211,12 @@ module Analysis =
                 match d with
                 | Import it ->
                     match it with
-                    | ImportDefault(_, l)
-                    | ImportEquals(_, l, _) ->
+                    | ImportDefault _ ->
+                        this.declDefault(ctx, d)
+                    | ImportEquals(_, l, r) ->
                         // Special case: we want the LHS name to map to the actual path.
                         // We do not want to call this.declDefault() which calls this.path().
-                        this.addPath(ctx.currentDecl, l.name, it.getPath())
+                        this.addPath(ctx.currentDecl, l, r)
                         [ d ]
                 | _ -> this.declDefault(ctx, d)
 
@@ -315,7 +316,7 @@ module Analysis =
                                                                     | ImportEquals(_, _, rhsDir) -> rhsDir.isAncestorOf(p)) imports
             let pInImportEqual = Option.fold (fun (p: Path) (im: ImportType) -> match im with
                                                                                 | ImportDefault _ -> p
-                                                                                | ImportEquals(_, lhsDir, rhsDir) -> Path(lhsDir.name::p.names[rhsDir.names.Length..])) p importEqual
+                                                                                | ImportEquals(_, lhsDir, rhsDir) -> Path(lhsDir::p.names[rhsDir.names.Length..])) p importEqual
             let pInCurrentMethod = currentMethodPath.relativize pInImportEqual
             // Note that directly returning "pInCurrentMethod" here is also OK in most cases.
             //

TypeInjections/TypeInjections/DafnyToYIL.fs
@@ -116,7 +116,7 @@ module DafnyToYIL =
             if d.GetCompileName(dafnyOptions) = d.Signature.ModuleDef.DafnyName then
                 [ Y.Import(Y.ImportDefault(d.Opened, pathOfModule (d.Signature.ModuleDef))) ]
             else
-                [ Y.Import(Y.ImportEquals(d.Opened, Y.Path [ d.GetCompileName(dafnyOptions) ], pathOfModule (d.Signature.ModuleDef))) ]
+                [ Y.Import(Y.ImportEquals(d.Opened, d.GetCompileName(dafnyOptions), pathOfModule (d.Signature.ModuleDef))) ]
         | :? TypeSynonymDecl as d ->
             // type synonyms and HOL-style subtype definitions
             let tpvars = typeParameter @ d.TypeArgs

TypeInjections/TypeInjections/Translation.fs
@@ -648,7 +648,8 @@ module Translation =
                     let inSpec =
                         InputSpec(
                             inputs,
-                            inputRequiresO @ inputsTranslations @ losslessAssumptions
+                            inputRequiresO
+                            @ inputsTranslations @ losslessAssumptions
                         )
 
                     // we don't need the ensures-conditions of the method
@@ -678,7 +679,8 @@ module Translation =
                     // but that is redundant due to our highly restricted treatment of classes
                     // New inputs' ensures becomes "output spec" here because "input spec" contains requires
                     // and "output spec" contains ensures.
-                    let outSpec = OutputSpec([], inputEnsuresN @ [ outputsTranslation ])
+                    let outSpec =
+                        OutputSpec([], inputEnsuresN @ [ outputsTranslation ])
 
                     // The body yields the proof of the lemma.
                     let proof =
@@ -1087,7 +1089,7 @@ module Translation =
             let pathOfOld =
                 fun (decl: Decl) -> Path [ nameD.getOld; decl.name ]
 
-            let mO = ctxOm.lookupCurrent()
+            let mO = ctxOm.lookupCurrent ()
 
             let childPaths = List.map pathOfOld mO.children
             let sameChildren = List.map pathOfOld (declD.getSame ())

TypeInjections/TypeInjections/Traverser.fs
@@ -65,9 +65,10 @@ module Traverser =
               meta = p.meta }
 
         member this.importType(ctx: Context, e: ImportType) =
+            // lhs in ImportEquals is just a name.
             match e with
             | ImportDefault (o, p) -> ImportDefault(o, this.path (ctx, p))
-            | ImportEquals (o, lhs, rhs) -> ImportEquals(o, this.path (ctx, lhs), this.path (ctx, rhs))
+            | ImportEquals (o, lhs, rhs) -> ImportEquals(o, lhs, this.path (ctx, rhs))
 
         member this.exportType(ctx: Context, e: ExportType) =
             let path p = this.path (ctx, p)
@@ -89,7 +90,7 @@ module Traverser =
                         ds
 
                 let moduleCtx =
-                    List.fold (fun (ctx: Context) -> ctx.addImport) (ctx.enter(n).clearImport()) imports
+                    List.fold (fun (ctx: Context) -> ctx.addImport) (ctx.enter(n).clearImport ()) imports
 
                 let membersT =
                     List.collect (fun (d: Decl) -> this.decl (moduleCtx, d)) ds

TypeInjections/TypeInjections/YIL.fs
@@ -360,7 +360,7 @@ module YIL =
 
     and ImportType =
         | ImportDefault of opened: bool * dir: Path
-        | ImportEquals of opened: bool * lhsDir: Path * rhsDir: Path
+        | ImportEquals of opened: bool * lhsDir: string * rhsDir: Path
         override this.ToString() =
             match this with
             | ImportDefault (o, p) -> "import " + (if o then "opened " else "") + p.ToString()

commit a980f42013249a8d8aa7b710bdcf52937bdea2cf

Fix a bug causing wrong path qualifier introduced in a561f98

TypeInjections/TypeInjections/Analysis.fs
@@ -306,8 +306,9 @@ module Analysis =
                 match d with
                 | Import it ->
                     match it with
-                    | ImportDefault _ ->
-                        this.declDefault(ctx, d)
+                    | ImportDefault (_, l) ->
+                        this.addPath(ctx.currentDecl, l.name, l)
+                        [ d ]
                     | ImportEquals(_, l, r) ->
                         // Special case: we want the LHS name to map to the actual path.
                         // We do not want to call this.declDefault() which calls this.path().